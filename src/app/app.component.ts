06f9cdd10f148252b430f03ce66033e31b122633
AuslinPJ <31402534+AuslinPJ@users.noreply.github.com>
2017-09-06 13:21:10 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+    id: number;
+    name: string;
+}
+const HEROES: Hero[] = [
+  { id: 1, name: 'Apple' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+ 
+];
+ 
+@Component({
+  selector: 'app-root',
+templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Creative';
+      heroes = HEROES;
+  selectedHero: Hero;
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
