a58e7b5fc0c9b62a6446d49888317d4415355684
AuslinPJ <31402534+AuslinPJ@users.noreply.github.com>
2017-09-09 20:49:34 +0530

    routing
    updated till routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
-
-
+import { HeroesComponent }     from './heroes.component';
+import {HeroService} from './hero.service';
 import { AppComponent } from './app.component';
+import { AppRoutingModule }   from './app-routing.module';
+import {DashboardComponent} from './dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
       BrowserModule,
       FormsModule,
-      
+      AppRoutingModule
   ],
-  providers: [],
+  providers: [ HeroService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,24 +1,31 @@
-import { Component , Input} from '@angular/core';
+import { Component , Input, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Location} from '@angular/common';
+import {HeroService} from './hero.service';
 import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'hero-detail',
-  template:`<div class="center" *ngIf="hero" >
-     
-      
-      <div>
-       Details for id {{hero.id}}
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-        `,
-       styles: [`
-        .center {
-           
-            text-align: center;
-      }        
-
-        `]
+  templateUrl:'./hero-detail.component.html',
+       styles: [`./hero-detail.component.css `]
 })
-export class HeroDetailComponent {
-@Input() hero: Hero;
+export class HeroDetailComponent implements OnInit {
+hero: Hero;
+
+constructor(
+  private heroService: HeroService,
+  private route: ActivatedRoute,
+  private location: Location
+) {}
+ngOnInit(): void {
+  this.route.paramMap
+    .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+    .subscribe(hero => this.hero = hero);
+ 
+}
+   goBack(): void {
+  this.location.back();
+}
+
 }
